0a459b6c2346eeebaa005421c357267f3b2a2039
Anna <ann1989@list.ru>
2022-07-20 22:51:00 +0300

    Homework

---
+++ Downloads/GeekBrains/С#/Seminar_3/Homework/Task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿// 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Примеры:
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))+((z2 - z1) * (z2 - z1)));
+
+// Решение 2:
+//double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)+ Math.Pow((z2 - z1), 2));
+//Console.WriteLine(string.Format("{0:F3}", distance));
+
+Console.WriteLine("Расстояние между точками равно " + distance);
